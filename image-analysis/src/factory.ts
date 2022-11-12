492197fa23428073ad82901fae72c658be6d072d
RenatoLomba <rntlomba@gmail.com>
2022-11-12 18:35:04 -0300

    feat: eslint config

--- image-analysis/src/factory.ts
+++ image-analysis/src/factory.ts
@@ -1,12 +1,10 @@
 import { Rekognition, Translate } from 'aws-sdk'
+
 import { Handler } from './handler'
 
 const rekognitionService = new Rekognition()
 const translateService = new Translate()
 
-const handler = new Handler(
-  rekognitionService, 
-  translateService 
-)
+const handler = new Handler(rekognitionService, translateService)
 
 export const main = handler.main.bind(handler)
--- image-analysis/src/handler.ts
+++ image-analysis/src/handler.ts
@@ -1,20 +1,24 @@
-import { Rekognition, Translate } from "aws-sdk"
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { Rekognition, Translate } from 'aws-sdk'
 
 export class Handler {
   constructor(
     private rekognitionService: Rekognition,
-    private translateService: Translate
-  ) { }
+    private translateService: Translate,
+  ) {}
 
   async main(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
     console.log('EVENT', event)
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        message: 'Hello Image Analysis!'
-      }, null, 2)
+      body: JSON.stringify(
+        {
+          message: 'Hello Image Analysis!',
+        },
+        null,
+        2,
+      ),
     }
   }
 }
