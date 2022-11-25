ea60d192fd48c8e823e1015610c649f59e9268ef
RenatoLomba <rntlomba@gmail.com>
2022-11-24 23:47:52 -0300

    add create table script to docker

--- v1-trigger-dynamodb-nest/scripts/create-dynamodb-table.sh
+++ v1-trigger-dynamodb-nest/scripts/create-dynamodb-table.sh
@@ -1,9 +1,15 @@
-DYNAMODB_TABLE_NAME=Heroes
+mkdir logs
 
 aws dynamodb create-table \
-  --table-name $DYNAMODB_TABLE_NAME \
+  --table-name $DYNAMODB_TABLE \
   --attribute-definitions AttributeName=id,AttributeType=S AttributeName=name,AttributeType=S \
   --key-schema AttributeName=id,KeyType=HASH  AttributeName=name,KeyType=RANGE \
   --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+  --stream-specification StreamEnabled=true,StreamViewType=NEW_AND_OLD_IMAGES \
   --region us-east-1 \
-  --endpoint-url=http://localhost:4566 | jq
+  --endpoint-url=http://$LOCALSTACK_HOST:4566 \
+  | tee logs/create-dynamodb-table.log
+
+DYNAMODB_TABLE_STREAM=$(cat logs/create-dynamodb-table.log | jq -r .TableDescription.LatestStreamArn)
+
+echo $DYNAMODB_TABLE_STREAM
